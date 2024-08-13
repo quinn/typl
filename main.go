ce8a03c01db660aaad3ac9f1eca03a5d99ccab7d
quinn <q.shanahan@gmail.com>
2024-08-12 22:37:50 -0400

    wip

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"text/template/parse"
 )
 
+type Field struct {
+	Name     string
+	Type     string
+	IsSlice  bool
+	Children map[string]*Field
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide the template file path as an argument")
@@ -23,9 +30,13 @@ func main() {
 	}
 
 	structName := getStructNameFromFileName(templatePath)
-	fields := extractFieldsFromTemplateAST(string(content))
+	fields, err := extractFieldsFromTemplateAST(string(content))
+	if err != nil {
+		fmt.Printf("Error extracting fields: %v\n", err)
+		return
+	}
 
-	generateStruct(structName, fields)
+	generateStructs(structName, fields)
 }
 
 func getStructNameFromFileName(filePath string) string {
@@ -33,53 +44,120 @@ func getStructNameFromFileName(filePath string) string {
 	return strings.Title(base) + "Input"
 }
 
-func extractFieldsFromTemplateAST(content string) map[string]string {
-	fields := make(map[string]string)
+func extractFieldsFromTemplateAST(content string) (map[string]*Field, error) {
+	fields := make(map[string]*Field)
 
 	tmpl, err := template.New("template").Parse(content)
 	if err != nil {
-		fmt.Printf("Error parsing template: %v\n", err)
-		return fields
+		return nil, fmt.Errorf("error parsing template: %v", err)
 	}
 
-	var extractFields func(node parse.Node)
-	extractFields = func(node parse.Node) {
+	var extractFields func(node parse.Node, currentFields map[string]*Field, inRange bool) error
+	extractFields = func(node parse.Node, currentFields map[string]*Field, inRange bool) error {
+		if node == nil {
+			return nil
+		}
+
 		switch n := node.(type) {
 		case *parse.ActionNode:
+			if n.Pipe == nil {
+				return nil
+			}
 			for _, cmd := range n.Pipe.Cmds {
 				for _, arg := range cmd.Args {
-					if field, ok := arg.(*parse.FieldNode); ok {
+					if field, ok := arg.(*parse.FieldNode); ok && len(field.Ident) > 0 {
 						fieldName := field.Ident[0]
-						if _, exists := fields[fieldName]; !exists {
-							fields[fieldName] = "string" // Default to string, you can enhance this later
+						if _, exists := currentFields[fieldName]; !exists {
+							currentFields[fieldName] = &Field{
+								Name:     fieldName,
+								Type:     "string", // Default to string, can be enhanced later
+								Children: make(map[string]*Field),
+							}
+						}
+						if inRange && len(field.Ident) > 1 {
+							// Handle nested fields in range
+							currentField := currentFields[fieldName]
+							for _, nestedField := range field.Ident[1:] {
+								if _, exists := currentField.Children[nestedField]; !exists {
+									currentField.Children[nestedField] = &Field{
+										Name:     nestedField,
+										Type:     "string",
+										Children: make(map[string]*Field),
+									}
+								}
+								currentField = currentField.Children[nestedField]
+							}
 						}
 					}
 				}
 			}
 		case *parse.ListNode:
-			for _, item := range n.Nodes {
-				extractFields(item)
+			if n != nil {
+				for _, item := range n.Nodes {
+					if err := extractFields(item, currentFields, inRange); err != nil {
+						return err
+					}
+				}
 			}
 		case *parse.IfNode:
-			extractFields(n.List)
-			extractFields(n.ElseList)
+			if err := extractFields(n.List, currentFields, inRange); err != nil {
+				return err
+			}
+			if err := extractFields(n.ElseList, currentFields, inRange); err != nil {
+				return err
+			}
 		case *parse.RangeNode:
-			extractFields(n.List)
-			extractFields(n.ElseList)
+			if len(n.Pipe.Decl) > 0 {
+				rangeVar := n.Pipe.Decl[0].String()
+				if _, exists := currentFields[rangeVar]; !exists {
+					currentFields[rangeVar] = &Field{
+						Name:     rangeVar,
+						Type:     rangeVar + "Item",
+						IsSlice:  true,
+						Children: make(map[string]*Field),
+					}
+				}
+				if err := extractFields(n.List, currentFields[rangeVar].Children, true); err != nil {
+					return err
+				}
+			}
+			if err := extractFields(n.ElseList, currentFields, inRange); err != nil {
+				return err
+			}
 		case *parse.WithNode:
-			extractFields(n.List)
-			extractFields(n.ElseList)
+			if err := extractFields(n.List, currentFields, inRange); err != nil {
+				return err
+			}
+			if err := extractFields(n.ElseList, currentFields, inRange); err != nil {
+				return err
+			}
 		}
+		return nil
 	}
 
-	extractFields(tmpl.Tree.Root)
-	return fields
+	if err := extractFields(tmpl.Tree.Root, fields, false); err != nil {
+		return nil, fmt.Errorf("error extracting fields: %v", err)
+	}
+
+	return fields, nil
 }
 
-func generateStruct(structName string, fields map[string]string) {
-	fmt.Printf("type %s struct {\n", structName)
-	for fieldName, fieldType := range fields {
-		fmt.Printf("\t%s %s\n", strings.Title(fieldName), fieldType)
+func generateStructs(structName string, fields map[string]*Field) {
+	generateStruct(structName, fields, 0)
+}
+
+func generateStruct(structName string, fields map[string]*Field, indent int) {
+	indentStr := strings.Repeat("\t", indent)
+	fmt.Printf("%stype %s struct {\n", indentStr, structName)
+	for _, field := range fields {
+		fieldType := field.Type
+		if field.IsSlice {
+			fieldType = "[]" + fieldType
+		}
+		fmt.Printf("%s\t%s %s\n", indentStr, strings.Title(field.Name), fieldType)
+		if len(field.Children) > 0 {
+			generateStruct(field.Type, field.Children, indent+1)
+		}
 	}
-	fmt.Println("}")
+	fmt.Printf("%s}\n\n", indentStr)
 }
